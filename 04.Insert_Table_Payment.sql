c140dcaf9b20acde6013ca751c4e2cbb1dd25bb5
AutomicDemoCAmena <61249931+AutomicDemoCAmena@users.noreply.github.com>
2020-10-26 22:24:49 +0400

    Update 04.Insert_Table_Payment.sql

--- 04.Insert_Table_Payment.sql
+++ 04.Insert_Table_Payment.sql
@@ -4,3 +4,4 @@ INSERT INTO Payment VALUES (3,'MasterCard',12002.25,'SAR','Phone',11);
 INSERT INTO Payment VALUES (4,'Cash',40.75,'AED','Bread',4);
 INSERT INTO Payment VALUES (5,'Cash',30.00,'AED','Coke',6);
 INSERT INTO Payment VALUES (6,'Cash',56.30,'USD','Chocolate',21);
+INSERT INTO Payment VALUES (6,'Cash',22.50,'AED','Pen',3);
